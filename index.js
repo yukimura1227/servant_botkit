f606c649b9655aa0aa69c4fa2fb5844ce2d0d66b
yukimura1227 <takamura1227@gmail.com>
2019-09-05 11:25:50 +0900

    feat: impements all message redirect_to random function

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-const { Botkit } = require('botkit');
-const { SlackAdapter } = require('botbuilder-adapter-slack');
+const Botkit = require('botkit');
 
-const adapter = new SlackAdapter({
-  //認証には、BotTokenだけでなく、verificationToken,
-  //あるいは clientSingleSecretToken のいづれかが必要です。
-  // verificationToken:process.env.SLACK_VERIFY,
-  botToken: process.env.SLACK_TOKEN
+if (!process.env.TOKEN) {
+  console.log('Error: Specify TOKEN in environment');
+  process.exit(1);
+}
+
+if (!process.env.SLACK_TEAM) {
+  console.log('Error: Specify SLACK_TEAM in environment');
+  process.exit(1);
+}
+
+const controller = Botkit.slackbot({
+  debug: false
 });
 
-let controller = new Botkit({
-    adapter: adapter
+controller.spawn({
+  token: process.env.TOKEN
+
+}).startRTM( function(err) {
+  if (err) {
+    throw new Error(err);
+  }
+
+});
+
+const hearing_event_all            = ['ambient'];
+const hearing_event_mention        = ['direct_mention', 'mention'];
+const hearing_event_direct_message = ['direct_message'];
+
+controller.hears('hi', hearing_event_mention, function(bot,message) {
+  bot.reply(message,'hi');
+});
+
+controller.hears('', hearing_event_all, function(bot,message) {
+  // console.log(bot);
+  console.log(message);
+  var id = message.client_msg_id;
+  var slack_team = process.env.SLACK_TEAM;
+  var channel_id = message.channel;
+  var event_ts = message.event_ts;
+  event_ts = event_ts.replace('.','')
+  var url_parameter = `?thread_ts=${message.thread_ts}&cid=${channel_id}`
+  var post_link = `https://${slack_team}.slack.com/archives/${channel_id}/p${event_ts}`
+  var channel_from_id
+  bot.api.chat.postMessage({
+    text: `${post_link}`,
+    channel: '#random',
+    as_user: false,
+    username: bot.identity.name,
+    unfurl_links: true
+  }, function(err, message){
+    if(err) { console.log("err: ", err); return; }
+  });
 });
